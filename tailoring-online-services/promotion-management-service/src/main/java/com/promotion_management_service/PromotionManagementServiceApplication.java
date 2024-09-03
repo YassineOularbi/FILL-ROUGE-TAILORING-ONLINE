8cd8b4d06a63bde944ba3967fba7e92f6869e096
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-03 20:21:58 +0100

    Add services of notification mailing & promotions

---
+++ tailoring-online-services/promotion-management-service/src/main/java/com/promotion_management_service/PromotionManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.promotion_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionManagementServiceApplication.class, args);
+	}
+
+}
