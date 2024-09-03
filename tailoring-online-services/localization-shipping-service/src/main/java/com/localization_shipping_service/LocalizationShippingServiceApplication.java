521a204d7c17da5cca99413bce154b1014a99a02
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-03 20:02:14 +0100

    Add localization & shipping service

---
+++ tailoring-online-services/localization-shipping-service/src/main/java/com/localization_shipping_service/LocalizationShippingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.localization_shipping_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizationShippingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizationShippingServiceApplication.class, args);
+	}
+
+}
---
+++ tailoring-online-services/localization-shipping-service/src/test/java/com/localization_shipping_service/LocalizationShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localization_shipping_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizationShippingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
