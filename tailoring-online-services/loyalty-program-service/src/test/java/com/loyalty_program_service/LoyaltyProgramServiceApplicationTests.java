6b4a88b8e6f8dca444af64440664975cab81c349
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-03 20:19:50 +0100

    Add services of loyalty program & promotions

---
+++ tailoring-online-services/loyalty-program-service/src/test/java/com/loyalty_program_service/LoyaltyProgramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyalty_program_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyProgramServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
