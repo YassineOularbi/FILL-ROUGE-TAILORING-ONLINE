d9cef70fa58aaa31cdb2575c520a6ddc0a109db1
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-03 20:05:10 +0100

    Add payment & banking service service

---
+++ tailoring-online-services/payment-banking-service/src/main/java/com/payment_banking_service/PaymentBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment_banking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBankingServiceApplication.class, args);
+	}
+
+}
