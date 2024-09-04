6b022ff40b2cf8e612dcbdb9e3966600c168486e
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-04 22:02:21 +0100

    Add logique of adding an address to user communicating by feignClient

---
+++ tailoring-online-services/localization-shipping-service/src/main/java/com/localization_shipping_service/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.localization_shipping_service.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String id) {
+        super(STR."User not found with id: \{id}");
+    }
+}
--- tailoring-online-services/localization-shipping-service/src/main/java/com/localization_shipping_service/service/AddressService.java
+++ tailoring-online-services/localization-shipping-service/src/main/java/com/localization_shipping_service/service/AddressService.java
@@ -1,7 +1,9 @@
 package com.localization_shipping_service.service;
 
+import com.localization_shipping_service.client.UserManagementClient;
 import com.localization_shipping_service.dto.AddressDto;
 import com.localization_shipping_service.exception.AddressNotFoundException;
+import com.localization_shipping_service.exception.UserNotFoundException;
 import com.localization_shipping_service.mapper.AddressMapper;
 import com.localization_shipping_service.model.Address;
 import com.localization_shipping_service.repository.AddressRepository;
@@ -18,6 +20,7 @@ public class AddressService {
 
     private final AddressRepository addressRepository;
     private final AddressMapper addressMapper;
+    private final UserManagementClient userManagementClient;
 
     public List<Address> getAllAddresses() {
         return addressRepository.findAll();
@@ -26,11 +29,22 @@ public class AddressService {
     public Address getAddressById(Long id) {
         return addressRepository.findById(id).orElseThrow(() -> new AddressNotFoundException(id));
     }
-
+    
     public AddressDto addAddress(AddressDto addressDto, String id) {
-        var address = addressMapper.toEntity(addressDto);
-        var savedAddress = addressRepository.save(address);
-        return addressMapper.toDto(savedAddress);
+        var user = userManagementClient.getUserById(id);
+        if (user.getStatusCode().is2xxSuccessful()){
+            var address = addressRepository.findByUserId(id);
+            if (address.isPresent()){
+                 return updateAddress(address.get().getId(), addressDto);
+            } else {
+                var mappedAddress = addressMapper.toEntity(addressDto);
+                mappedAddress.setUserId(id);
+                var savedAddress = addressRepository.save(mappedAddress);
+                return addressMapper.toDto(savedAddress);
+            }
+        } else {
+            throw new UserNotFoundException(id);
+        }
     }
 
     public AddressDto updateAddress(Long id, AddressDto addressDto) {
