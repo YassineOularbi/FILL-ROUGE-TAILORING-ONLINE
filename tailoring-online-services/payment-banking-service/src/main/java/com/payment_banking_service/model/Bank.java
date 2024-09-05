7e0a66384825eb4541ba7d7352981f9ee1312111
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-05 03:07:30 +0100

    Add service of banking

--- tailoring-online-services/payment-banking-service/src/main/java/com/payment_banking_service/model/Bank.java
+++ tailoring-online-services/payment-banking-service/src/main/java/com/payment_banking_service/model/Bank.java
@@ -1,38 +1,31 @@
-//package com.user_management_service.model;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.io.Serializable;
-//import java.sql.Date;
-//
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@Table(name = "bank")
-//public class Bank implements Serializable {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(name = "card_number", nullable = false, unique = true)
-//    private String cardNumber;
-//
-//    @Column(name = "expiration_date", nullable = false)
-//    private Date expirationDate;
-//
-//    @Column(name = "cvc", nullable = false)
-//    private String cvc;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "billing_address_id")
-//    private Address billingAddress;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "user_id", nullable = false)
-//    private User user;
-//}
+package com.payment_banking_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "bank")
+public class Bank implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "card_number", nullable = false, unique = true)
+    private String cardNumber;
+
+    @Column(name = "expiration_date", nullable = false)
+    private Date expirationDate;
+
+    @Column(name = "cvc", nullable = false)
+    private String cvc;
+
+}
--- tailoring-online-services/payment-banking-service/src/main/java/com/payment_banking_service/service/BankService.java
+++ tailoring-online-services/payment-banking-service/src/main/java/com/payment_banking_service/service/BankService.java
@@ -1,52 +1,47 @@
-//package com.user_management_service.service;
-//
-//import com.user_management_service.dto.BankDto;
-//import com.user_management_service.exception.BankNotFoundException;
-//import com.user_management_service.exception.UserNotFoundException;
-//import com.user_management_service.mapper.BankMapper;
-//import com.user_management_service.model.Bank;
-//import com.user_management_service.repository.BankRepository;
-//import com.user_management_service.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class BankService {
-//
-//    private final BankRepository bankRepository;
-//    private final BankMapper bankMapper;
-//    private final UserRepository userRepository;
-//
-//    public List<Bank> getAllBanks() {
-//        return bankRepository.findAll();
-//    }
-//
-//    public Bank getBankById(Long id) {
-//        return bankRepository.findById(id).orElseThrow(() -> new BankNotFoundException(id));
-//    }
-//
-//    public BankDto addBank(BankDto bankDto, String id) {
-//        var user = userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id));
-//        var bank = bankMapper.toEntity(bankDto);
-//        bank.setUser(user);
-//        var savedBank = bankRepository.save(bank);
-//        return bankMapper.toDto(savedBank);
-//    }
-//
-//    public BankDto updateBank(Long id, BankDto bankDto) {
-//        var existingBank = bankRepository.findById(id).orElseThrow(() -> new BankNotFoundException(id));
-//        var updatedBank = bankMapper.partialUpdate(bankDto, existingBank);
-//        var savedBank = bankRepository.save(updatedBank);
-//        return bankMapper.toDto(savedBank);
-//    }
-//
-//    public void deleteBank(Long id) {
-//        var bank = bankRepository.findById(id).orElseThrow(() -> new BankNotFoundException(id));
-//        bankRepository.delete(bank);
-//    }
-//}
+package com.payment_banking_service.service;
+
+import com.payment_banking_service.dto.BankDto;
+import com.payment_banking_service.exception.BankNotFoundException;
+import com.payment_banking_service.mapper.BankMapper;
+import com.payment_banking_service.model.Bank;
+import com.payment_banking_service.repository.BankRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BankService {
+
+    private final BankRepository bankRepository;
+    private final BankMapper bankMapper;
+
+    public List<Bank> getAllBanks() {
+        return bankRepository.findAll();
+    }
+
+    public Bank getBankById(Long id) {
+        return bankRepository.findById(id).orElseThrow(() -> new BankNotFoundException(id));
+    }
+
+    public BankDto addBank(BankDto bankDto, String id) {
+        var bank = bankMapper.toEntity(bankDto);
+        var savedBank = bankRepository.save(bank);
+        return bankMapper.toDto(savedBank);
+    }
+
+    public BankDto updateBank(Long id, BankDto bankDto) {
+        var existingBank = bankRepository.findById(id).orElseThrow(() -> new BankNotFoundException(id));
+        var updatedBank = bankMapper.partialUpdate(bankDto, existingBank);
+        var savedBank = bankRepository.save(updatedBank);
+        return bankMapper.toDto(savedBank);
+    }
+
+    public void deleteBank(Long id) {
+        var bank = bankRepository.findById(id).orElseThrow(() -> new BankNotFoundException(id));
+        bankRepository.delete(bank);
+    }
+}
