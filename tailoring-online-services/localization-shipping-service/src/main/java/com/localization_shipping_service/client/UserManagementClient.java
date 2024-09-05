d057c5af859faa74779c56d788a8f5ef0189c63b
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-05 03:03:06 +0100

    Add api gateway but not yet working

--- tailoring-online-services/localization-shipping-service/src/main/java/com/localization_shipping_service/client/UserManagementClient.java
+++ tailoring-online-services/localization-shipping-service/src/main/java/com/localization_shipping_service/client/UserManagementClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "user-management-service", url = "http://localhost:8081/users")
 public interface UserManagementClient {
 
-    @GetMapping("/get-user-by-id/{id}")
+    @GetMapping("/api/user/get-user-by-id/{id}")
     ResponseEntity<?> getUserById(@PathVariable("id") String id);
 }
