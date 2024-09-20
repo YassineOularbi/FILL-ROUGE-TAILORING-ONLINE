cc61f544430a3eb89659bbced78d248f4e3b760b
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-20 22:30:17 +0100

    Add collections as designed in figma

--- tailoring-online-interfaces/src/app/features/home/home.component.ts
+++ tailoring-online-interfaces/src/app/features/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { MainComponent } from './_components/main/main.component';
+import { CollectionsComponent } from './_components/collections/collections.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    CollectionsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
