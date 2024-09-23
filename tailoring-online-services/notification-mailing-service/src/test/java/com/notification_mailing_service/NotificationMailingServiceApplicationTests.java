e17893d047ec272590dc68301355a41a4a412f42
YassineOularbi <yassineoularbi4@gmail.com>
2024-09-23 16:20:28 +0100

    Add Test unitaire of service notification

--- tailoring-online-services/notification-mailing-service/src/test/java/com/notification_mailing_service/NotificationMailingServiceApplicationTests.java
+++ tailoring-online-services/notification-mailing-service/src/test/java/com/notification_mailing_service/NotificationMailingServiceApplicationTests.java
@@ -1,13 +1,4 @@
 package com.notification_mailing_service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class NotificationMailingServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
