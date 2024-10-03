e0caf935c098fbf876d665a3a24afcdddc2547f5
YassineOularbi <yassineoularbi4@gmail.com>
2024-10-03 19:36:25 +0100

    Add logging for logstash elasticsearch to store management

--- tailoring-online-services/store-management-service/src/main/java/com/store_management_service/client/UserManagementClient.java
+++ tailoring-online-services/store-management-service/src/main/java/com/store_management_service/client/UserManagementClient.java
@@ -1,7 +1,6 @@
 package com.store_management_service.client;
 
 
-import com.store_management_service.config.FeignClientConfig;
 import com.store_management_service.model.Tailor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
