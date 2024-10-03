0247ce716a0468337882d93ce347fdf0ecd5d18d
YassineOularbi <yassineoularbi4@gmail.com>
2024-10-03 19:37:45 +0100

    Add logging for logstash elasticsearch to user management

--- tailoring-online-services/user-management-service/src/main/java/com/user_management_service/client/NotificationMailingClient.java
+++ tailoring-online-services/user-management-service/src/main/java/com/user_management_service/client/NotificationMailingClient.java
@@ -1,6 +1,5 @@
 package com.user_management_service.client;
 
-import com.user_management_service.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
