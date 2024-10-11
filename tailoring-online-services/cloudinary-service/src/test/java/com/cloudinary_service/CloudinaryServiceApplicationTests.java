63c1c2c3e34a715104b9df60edf5eaaa6c006a02
YassineOularbi <yassineoularbi4@gmail.com>
2024-10-11 15:24:34 +0100

    Add cloudinary service in backend for managing images & videos

---
+++ tailoring-online-services/cloudinary-service/src/test/java/com/cloudinary_service/CloudinaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudinary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudinaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
