0c058b46e0126214350e9d53c8c310c7f38c9ee7
YassineOularbi <yassineoularbi4@gmail.com>
2024-10-13 00:39:58 +0100

    Add footer to shop component tailoring-online

--- tailoring-online-interfaces/src/app/features/shop/shop.component.ts
+++ tailoring-online-interfaces/src/app/features/shop/shop.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { HeroComponent } from "./_components/hero/hero.component";
+import { FooterComponent } from './_components/footer/footer.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, FooterComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
