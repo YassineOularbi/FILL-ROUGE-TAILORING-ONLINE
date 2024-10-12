5edd2a657dd739137eab34d137801c65abdef1ab
YassineOularbi <yassineoularbi4@gmail.com>
2024-10-13 00:27:18 +0100

    Add hero section of store tailoring online

--- tailoring-online-interfaces/src/app/features/shop/shop.component.ts
+++ tailoring-online-interfaces/src/app/features/shop/shop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { HeroComponent } from "./_components/hero/hero.component";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
