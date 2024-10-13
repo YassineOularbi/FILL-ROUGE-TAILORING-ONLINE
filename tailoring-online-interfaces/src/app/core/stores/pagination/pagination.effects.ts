f8d78b4f16aaeadcaffcab70dfeb95b76bef122a
YassineOularbi <yassineoularbi4@gmail.com>
2024-10-13 03:24:47 +0100

    Provide l'injection des stores for the routes specifics for some routes or components or children

--- tailoring-online-interfaces/src/app/core/stores/pagination/pagination.effects.ts
+++ tailoring-online-interfaces/src/app/core/stores/pagination/pagination.effects.ts
@@ -1,11 +1,12 @@
+// src/app/features/shop/store/effects/pagination.effects.ts
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as PaginationActions from './pagination.actions';
-import { mergeMap, map, catchError, withLatestFrom, tap } from 'rxjs/operators';
+import { mergeMap, map, catchError, tap, withLatestFrom } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { PaginationState } from './pagination.reducer';
-import { selectPaginationState } from './pagination.selectors';
+import { selectPage, selectPageSize, selectSortField, selectSortDirection } from './pagination.selectors';
 
 @Injectable()
 export class PaginationEffects {
@@ -45,13 +46,22 @@ export class PaginationEffects {
         PaginationActions.setPageSize,
         PaginationActions.setSort
       ),
-      withLatestFrom(this.store.select(selectPaginationState)),
-      tap(([action, state]) => {
+      withLatestFrom(
+        this.store.select(selectPage),
+        this.store.select(selectPageSize),
+        this.store.select(selectSortField),
+        this.store.select(selectSortDirection)
+      ),
+      tap(([action, page, size, sortField, sortDirection]) => {
+        const state: PaginationState = { page, size, sortField, sortDirection, loading: false, error: null };
         localStorage.setItem('pagination', JSON.stringify(state));
       })
     ),
     { dispatch: false }
   );
 
-  constructor(private actions$: Actions, private store: Store<PaginationState>) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<PaginationState>
+  ) {}
 }
--- tailoring-online-interfaces/src/app/features/shop/shop.routes.ts
+++ tailoring-online-interfaces/src/app/features/shop/shop.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { paginationReducer } from '../../core/stores/pagination/pagination.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { PaginationEffects } from '../../core/stores/pagination/pagination.effects';
 
 export const SHOP_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () => import('./shop.component').then(m => m.ShopComponent),
+    providers: [
+        provideStore({ pagination: paginationReducer }),
+        provideEffects([PaginationEffects])
+      ]
   }
 ];
