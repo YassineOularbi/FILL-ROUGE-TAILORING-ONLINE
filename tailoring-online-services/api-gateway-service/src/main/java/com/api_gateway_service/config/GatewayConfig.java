23e487184c0a315e7900f57252686c9ab92ec071
Yassine Oularbi <yassineoularbi4@gmail.com>
2024-12-22 22:59:05 +0100

    Customize gateway server with oauth2 client config & add the tokenRelay GatewayFactory filter to manage tokens & tested the load balancer with multiple instnces

--- tailoring-online-services/api-gateway-service/src/main/java/com/api_gateway_service/config/GatewayConfig.java
+++ tailoring-online-services/api-gateway-service/src/main/java/com/api_gateway_service/config/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +21,10 @@ public class GatewayConfig {
     @Bean
     public DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rd, DiscoveryLocatorProperties dl) {
         return new DiscoveryClientRouteDefinitionLocator(rd, dl);
+    }
+
+    @Bean
+    public GlobalFilter tokenRelayGlobalFilter(TokenRelayGatewayFilterFactory tokenRelayGatewayFilterFactory) {
+        return (exchange, chain) -> tokenRelayGatewayFilterFactory.apply().filter(exchange, chain);
     }
 }
